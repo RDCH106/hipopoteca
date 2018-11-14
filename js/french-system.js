ab15a08857bebfb28e82991a53db83d5c38e8c73
Rubén de Celis Hernández <contact@rdch106.hol.es>
2018-11-14 20:53:15 +0100

    Fix used var

--- js/french-system.js
+++ js/french-system.js
@@ -1,8 +1,8 @@
 
 function monthlyInterest(quota, decimals=3){
-    return (quota/12).toFixed(precision);
+    return (quota/12).toFixed(decimals);
 }
 
 function numberOfQuotes(term, decimals=3){
-    return (term*12).toFixed(precision);
+    return (term*12).toFixed(decimals);
 }
