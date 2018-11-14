f8256c30fd3e7540ccd421bcfe44b5867bc1c804
Rubén de Celis Hernández <contact@rdch106.hol.es>
2018-11-14 20:47:01 +0100

    Add numerOfQuotes function

--- js/french-system.js
+++ js/french-system.js
@@ -2,3 +2,7 @@
 function monthlyInterest(quota, precision=8){
     return (quota/12).toPrecision(precision);
 }
+
+function numberOfQuotes(term, precision=8){
+    return (term*12).toPrecision(precision);
+}
