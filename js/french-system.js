0c7cd20db454b217aaacd10602eb443e573c12af
Rubén de Celis Hernández <contact@rdch106.hol.es>
2018-11-18 22:31:57 +0100

    Add export FrenchSystem

--- js/french-system.js
+++ js/french-system.js
@@ -19,3 +19,12 @@ function totalToPay(quota, numQuotas, decimals=3){
 function interest(totalToPay, capital, decimals=3){
     return (capital-totalToPay).toFixed(decimals);
 }
+
+export const default_decimals = 3;
+export var FrenchSystem = {
+    monthlyInterest: function (interest, decimals=default_decimals) { monthlyInterest(interest, decimals); },
+    quota: function (monthlyInterest, decimals=default_decimals) { quota(monthlyInterest, decimals); },
+    numberOfQuotes: function (term, decimals=default_decimals) { numberOfQuotes(term, decimals); },
+    totalToPay: function (quota, numQuotas, decimals=default_decimals) { totalToPay(quota, numQuotas, decimals); },
+    interest: function (totalToPay, capital, decimals=default_decimals) { interest(totalToPay, capital, decimals); }
+};
