6aca7beae2471d12a15e84040a7ac9060a12c7bb
Rubén de Celis Hernández <contact@rdch106.hol.es>
2018-11-24 18:54:29 +0100

    Format totalToPay value

--- js/calculate.js
+++ js/calculate.js
@@ -9,7 +9,7 @@ function calculateInput(){
     var quotaValue = quota(monthlyInterestValue, numberOfQuotesValue, capitalValue, 2);
     document.getElementById("quotaValue").innerHTML = quotaValue;
     var totalToPayValue = totalToPay(quotaValue, numberOfQuotesValue, 2);
-    document.getElementById("totalToPayValue").innerHTML = totalToPayValue;
+    document.getElementById("totalToPayValue").innerHTML = format("#.###,###", totalToPayValue);
     var interestValue = interest(totalToPayValue, capitalValue, 2);
     document.getElementById("interestValue").innerHTML = format("#.###,###", interestValue);
 }
