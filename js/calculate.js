2423e6d8fadf92c9d91f1585d5fb8cedced6df18
Rubén de Celis Hernández <contact@rdch106.hol.es>
2018-11-26 01:22:00 +0100

    Update calculate behaviour. Only calculate if form inputs are valid

--- js/calculate.js
+++ js/calculate.js
@@ -4,18 +4,20 @@ function calculateInput(){
     if($("#form-input")[0].checkValidity()){
         $("#form-result").fadeIn();
         //document.getElementById("form-result").style.display = "initial";
+    }
+    if($('#form-input').length > 0 && $("#form-input")[0].checkValidity()){
+        var capitalValue = document.getElementById('capital').value;
+        var monthlyInterestValue = monthlyInterest(document.getElementById('fixedType').value);
+        document.getElementById("monthlyInterestValue").innerHTML = monthlyInterestValue;
+        var numberOfQuotesValue = numberOfQuotes(document.getElementById('term').value);
+        document.getElementById("numberOfQuotesValue").innerHTML = numberOfQuotesValue;
+        var quotaValue = quota(monthlyInterestValue, numberOfQuotesValue, capitalValue, 2);
+        document.getElementById("quotaValue").innerHTML = format("##0.##0,#0", quotaValue);
+        var totalToPayValue = totalToPay(quotaValue, numberOfQuotesValue, 2);
+        document.getElementById("totalToPayValue").innerHTML = format("##0.##0,#0", totalToPayValue);
+        var interestValue = interest(totalToPayValue, capitalValue, 2);
+        document.getElementById("interestValue").innerHTML = format("##0.##0,#0", interestValue);
     }    
-    var capitalValue = document.getElementById('capital').value;
-    var monthlyInterestValue = monthlyInterest(document.getElementById('fixedType').value);
-    document.getElementById("monthlyInterestValue").innerHTML = monthlyInterestValue;
-    var numberOfQuotesValue = numberOfQuotes(document.getElementById('term').value);
-    document.getElementById("numberOfQuotesValue").innerHTML = numberOfQuotesValue;
-    var quotaValue = quota(monthlyInterestValue, numberOfQuotesValue, capitalValue, 2);
-    document.getElementById("quotaValue").innerHTML = format("##0.##0,#0", quotaValue);
-    var totalToPayValue = totalToPay(quotaValue, numberOfQuotesValue, 2);
-    document.getElementById("totalToPayValue").innerHTML = format("##0.##0,#0", totalToPayValue);
-    var interestValue = interest(totalToPayValue, capitalValue, 2);
-    document.getElementById("interestValue").innerHTML = format("##0.##0,#0", interestValue);
 }
 
 // On LOAD events
